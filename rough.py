e35266a6cc5d0b9e65688844a9f97f7010e9b3cc
pritam Ghosh <pgtd2002@gmail.com>
2026-04-17 07:59:01 +0530

    modifications

--- rough.py
+++ rough.py
@@ -1,3 +1,13 @@
 from functions.concatenation import build_concatenated_gkp_generator_qqpp
+import numpy as np
+from functions.binary_stabilizer_generators import shor_code_913
+from functions.binary_stabilizer_generators import repetition_code_XXZZ
+from functions.lattice import *
 
-from functions.
+x=repetition_code_XXZZ(2, 'X')
+y=[x,[0,0,0,0]]
+z,l=build_concatenated_gkp_generator_qqpp(x)
+m=qpqp_to_qqpp_permutation(2)
+print(l)
+print(x)
+print(z)
